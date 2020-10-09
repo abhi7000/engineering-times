44fcafdfc1b51bd03b8b3357c3d83e79ef7c4e2d
techloveabhi <abhi998979@gmail.com>
2020-10-09 21:12:52 +0530

    change in model filter

--- Jobs/serializers.py
+++ Jobs/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 from .models import Job
 class JobSerializers(serializers.ModelSerializer):
 	class Meta:
-		Model=Job
-		Field={'company_name','expire_date_time'}
+		model=Job
+		fields=['company_name','expiry_date_time']
--- Jobs/views.py
+++ Jobs/views.py
@@ -2,9 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import JobSerializers
 from .models import Job
+import datetime
+
 # Create your views here.
 @api_view(['GET'])
 def expire(request):
-	obj=Job.objects.all()
+	obj=Job.objects.filter(expiry_date_time__date__gt=datetime.date(2020, 10, 10))
 	serializer=JobSerializers(obj,many=True)
 	return Response(serializer.data)
